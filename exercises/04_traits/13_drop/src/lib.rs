76ecd7f00823ea684630d5954b2f55f116e66265
Sheng-Yan, Zhang <yan.code@gmail.com>
2024-06-11 15:48:36 +0800

    answer:01_enum

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,26 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+pub struct DropBomb {
+    defused: bool,
+}
+
+impl DropBomb {
+    pub fn new() -> Self {
+        DropBomb { defused: false }
+    }
+    pub fn defuse(&mut self) {
+        self.defused = true
+    }
+}
+impl Drop for DropBomb{
+    fn drop(&mut self){
+        if !self.defused{
+            panic!("Alahur akbar")
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,7 +29,7 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_drop_bomb() {
-        let bomb = DropBomb::new();
+        let _bomb = DropBomb::new();
         // The bomb should panic when dropped
     }
 
