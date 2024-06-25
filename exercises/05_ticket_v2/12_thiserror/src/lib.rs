6eb191949b2ba260e6c962cae34a948c2246871f
Sheng-Yan, Zhang <yan.code@gmail.com>
2024-06-25 09:48:02 +0800

    anwser:05:12 this error

--- exercises/05_ticket_v2/12_thiserror/src/lib.rs
+++ exercises/05_ticket_v2/12_thiserror/src/lib.rs
@@ -2,84 +2,87 @@
 //   We've changed the enum variants to be more specific, thus removing the need for storing
 //   a `String` field into each variant.
 //   You'll also have to add `thiserror` as a dependency in the `Cargo.toml` file.
-
+#[derive(Debug, thiserror::Error)]
 enum TicketNewError {
-    TitleCannotBeEmpty,
-    TitleTooLong,
-    DescriptionCannotBeEmpty,
-    DescriptionTooLong,
+  #[error("Title cannot be empty")]
+  TitleCannotBeEmpty,
+  #[error("Title cannot be longer than 50 bytes")]
+  TitleTooLong,
+  #[error("Description cannot be empty")]
+  DescriptionCannotBeEmpty,
+  #[error("Description cannot be longer than 500 bytes")]
+  DescriptionTooLong,
 }
-
 #[derive(Debug, PartialEq, Clone)]
 struct Ticket {
-    title: String,
-    description: String,
-    status: Status,
+  title: String,
+  description: String,
+  status: Status,
 }
 
 #[derive(Debug, PartialEq, Clone)]
 enum Status {
-    ToDo,
-    InProgress { assigned_to: String },
-    Done,
+  ToDo,
+  InProgress { assigned_to: String },
+  Done,
 }
 
 impl Ticket {
-    pub fn new(
-        title: String,
-        description: String,
-        status: Status,
-    ) -> Result<Ticket, TicketNewError> {
-        if title.is_empty() {
-            return Err(TicketNewError::TitleCannotBeEmpty);
-        }
-        if title.len() > 50 {
-            return Err(TicketNewError::TitleTooLong);
-        }
-        if description.is_empty() {
-            return Err(TicketNewError::DescriptionCannotBeEmpty);
-        }
-        if description.len() > 500 {
-            return Err(TicketNewError::DescriptionTooLong);
-        }
-
-        Ok(Ticket {
-            title,
-            description,
-            status,
-        })
+  pub fn new(
+    title: String,
+    description: String,
+    status: Status,
+  ) -> Result<Ticket, TicketNewError> {
+    if title.is_empty() {
+      return Err(TicketNewError::TitleCannotBeEmpty);
     }
+    if title.len() > 50 {
+      return Err(TicketNewError::TitleTooLong);
+    }
+    if description.is_empty() {
+      return Err(TicketNewError::DescriptionCannotBeEmpty);
+    }
+    if description.len() > 500 {
+      return Err(TicketNewError::DescriptionTooLong);
+    }
+    
+    Ok(Ticket {
+      title,
+      description,
+      status,
+    })
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use common::{overly_long_description, overly_long_title, valid_description, valid_title};
-
-    #[test]
-    fn title_cannot_be_empty() {
-        let err = Ticket::new("".into(), valid_description(), Status::ToDo).unwrap_err();
-        assert_eq!(err.to_string(), "Title cannot be empty");
-    }
-
-    #[test]
-    fn description_cannot_be_empty() {
-        let err = Ticket::new(valid_title(), "".into(), Status::ToDo).unwrap_err();
-        assert_eq!(err.to_string(), "Description cannot be empty");
-    }
-
-    #[test]
-    fn title_cannot_be_longer_than_fifty_chars() {
-        let err = Ticket::new(overly_long_title(), valid_description(), Status::ToDo).unwrap_err();
-        assert_eq!(err.to_string(), "Title cannot be longer than 50 bytes");
-    }
-
-    #[test]
-    fn description_cannot_be_too_long() {
-        let err = Ticket::new(valid_title(), overly_long_description(), Status::ToDo).unwrap_err();
-        assert_eq!(
-            err.to_string(),
-            "Description cannot be longer than 500 bytes"
-        );
-    }
+  use super::*;
+  use common::{overly_long_description, overly_long_title, valid_description, valid_title};
+  
+  #[test]
+  fn title_cannot_be_empty() {
+    let err = Ticket::new("".into(), valid_description(), Status::ToDo).unwrap_err();
+    assert_eq!(err.to_string(), "Title cannot be empty");
+  }
+  
+  #[test]
+  fn description_cannot_be_empty() {
+    let err = Ticket::new(valid_title(), "".into(), Status::ToDo).unwrap_err();
+    assert_eq!(err.to_string(), "Description cannot be empty");
+  }
+  
+  #[test]
+  fn title_cannot_be_longer_than_fifty_chars() {
+    let err = Ticket::new(overly_long_title(), valid_description(), Status::ToDo).unwrap_err();
+    assert_eq!(err.to_string(), "Title cannot be longer than 50 bytes");
+  }
+  
+  #[test]
+  fn description_cannot_be_too_long() {
+    let err = Ticket::new(valid_title(), overly_long_description(), Status::ToDo).unwrap_err();
+    assert_eq!(
+      err.to_string(),
+      "Description cannot be longer than 500 bytes"
+    );
+  }
 }
