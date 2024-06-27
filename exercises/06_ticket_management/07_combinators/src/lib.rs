2effb59c1153d6a79f4904ead38308d6f357d6c4
Sheng-Yan, Zhang <yan.code@gmail.com>
2024-06-27 09:19:28 +0800

    answer:06:07 combinatior

--- exercises/06_ticket_management/07_combinators/src/lib.rs
+++ exercises/06_ticket_management/07_combinators/src/lib.rs
@@ -4,60 +4,65 @@ use ticket_fields::{TicketDescription, TicketTitle};
 
 #[derive(Clone)]
 pub struct TicketStore {
-    tickets: Vec<Ticket>,
+  tickets: Vec<Ticket>,
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Ticket {
-    pub title: TicketTitle,
-    pub description: TicketDescription,
-    pub status: Status,
+  pub title: TicketTitle,
+  pub description: TicketDescription,
+  pub status: Status,
 }
 
 #[derive(Clone, Debug, Copy, PartialEq)]
 pub enum Status {
-    ToDo,
-    InProgress,
-    Done,
+  ToDo,
+  InProgress,
+  Done,
 }
 
 impl TicketStore {
-    pub fn new() -> Self {
-        Self {
-            tickets: Vec::new(),
-        }
+  pub fn new() -> Self {
+    Self {
+      tickets: Vec::new(),
     }
-
-    pub fn add_ticket(&mut self, ticket: Ticket) {
-        self.tickets.push(ticket);
-    }
+  }
+  pub fn to_dos(&self) -> Vec<&Ticket> {
+    self.tickets.iter()
+      .filter(|ticket| ticket.status == Status::ToDo)
+      .collect()
+  }
+  
+  pub fn add_ticket(&mut self, ticket: Ticket) {
+    self.tickets.push(ticket);
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use ticket_fields::test_helpers::{ticket_description, ticket_title};
-
-    #[test]
-    fn todos() {
-        let mut store = TicketStore::new();
-
-        let todo = Ticket {
-            title: ticket_title(),
-            description: ticket_description(),
-            status: Status::ToDo,
-        };
-        store.add_ticket(todo.clone());
-
-        let ticket = Ticket {
-            title: ticket_title(),
-            description: ticket_description(),
-            status: Status::InProgress,
-        };
-        store.add_ticket(ticket);
-
-        let todos: Vec<&Ticket> = store.to_dos();
-        assert_eq!(todos.len(), 1);
-        assert_eq!(todos[0], &todo);
-    }
+  use super::*;
+  use ticket_fields::test_helpers::{ticket_description, ticket_title};
+  
+  #[test]
+  fn todos() {
+    let mut store = TicketStore::new();
+    
+    let todo = Ticket {
+      title: ticket_title(),
+      description: ticket_description(),
+      status: Status::ToDo,
+    };
+    store.add_ticket(todo.clone());
+    
+    let ticket = Ticket {
+      title: ticket_title(),
+      description: ticket_description(),
+      status: Status::InProgress,
+    };
+    store.add_ticket(ticket);
+    
+    let todos: Vec<&Ticket> = store.to_dos();
+    assert_eq!(todos.len(), 1);
+    assert_eq!(todos[0], &todo);
+  }
 }
