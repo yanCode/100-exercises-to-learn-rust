3d35ea155bb5746b54611bbac8da77bb56a61baa
Sheng-Yan, Zhang <yan.code@gmail.com>
2024-06-30 15:37:46 +0800

    answer:06:10

--- exercises/06_ticket_management/10_slices/src/lib.rs
+++ exercises/06_ticket_management/10_slices/src/lib.rs
@@ -1,31 +1,34 @@
 // TODO: Define a function named `sum` that takes a reference to a slice of `u32` and returns the sum of all
 //  elements in the slice.
+pub fn sum(v: &[u32]) -> u32 {
+  v.iter().sum()
+}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn empty() {
-        let v = vec![];
-        assert_eq!(sum(&v), 0);
-    }
-
-    #[test]
-    fn one_element() {
-        let v = vec![1];
-        assert_eq!(sum(&v), 1);
-    }
-
-    #[test]
-    fn multiple_elements() {
-        let v = vec![1, 2, 3, 4, 5];
-        assert_eq!(sum(&v), 15);
-    }
-
-    #[test]
-    fn array_slice() {
-        let v = [1, 2, 3, 4, 5];
-        assert_eq!(sum(&v), 15);
-    }
+  use super::*;
+  
+  #[test]
+  fn empty() {
+    let v = vec![];
+    assert_eq!(sum(&v), 0);
+  }
+  
+  #[test]
+  fn one_element() {
+    let v = vec![1];
+    assert_eq!(sum(&v), 1);
+  }
+  
+  #[test]
+  fn multiple_elements() {
+    let v = vec![1, 2, 3, 4, 5];
+    assert_eq!(sum(&v), 15);
+  }
+  
+  #[test]
+  fn array_slice() {
+    let v = [1, 2, 3, 4, 5];
+    assert_eq!(sum(&v), 15);
+  }
 }
